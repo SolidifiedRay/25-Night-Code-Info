ece4e2877ff4751ab70288a1cbf555eecbfd34af
SolidifiedRay <yc3346@nyu.edu>
2021-03-08 20:42:45 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+      你好，世界
+    </Button>
+  );
+}
+
+export default App;
