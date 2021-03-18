d08d89f535f4584824ebdaebfe5442bcab3fc862
SolidifiedRay <yc3346@nyu.edu>
2021-03-18 18:50:55 -0400

    压缩其他节目图片并重命名

--- src/db/seiyuEventData.jsx
+++ src/db/seiyuEventData.jsx
@@ -1,21 +1,21 @@
 export const seiyuEventData = [
   {
     id: 'seiyu001',
-    path: '003.png',
-    title: 'プロセカアフタートーク 満たされないペイルカラー編0',
+    path: '20210226.png',
+    title: 'プロセカアフタートーク 満たされないペイルカラー編',
     subtitle: '铃木实里',
     link: 'https://www.bilibili.com/video/BV18A411K7De',
   },
   {
     id: 'seiyu002',
-    path: '002.png',
+    path: '20210213.png',
     title: 'プロセカストーリーシアター 25時、ナイトコードで。編',
     subtitle: '铃木实里 x 佐藤日向',
     link: 'https://www.bilibili.com/video/BV1Ab4y1R775',
   },
   {
     id: 'seiyu001',
-    path: '001.png',
+    path: '20210125.png',
     title: 'プロジェクトセカイ ワンダショちゃんねる #4',
     subtitle: '铃木实里',
     link: 'https://www.bilibili.com/video/BV1Wv4y1f7Sx',
